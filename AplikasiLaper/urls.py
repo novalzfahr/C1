1dac1a36a72eea83ebaa1da79416828970754a5e
Muhammad Faras Baginda <muhammad.faras@ui.ac.id>
2023-12-11 19:36:30 +0700

    menuadmin finish

--- AplikasiLaper/urls.py
+++ AplikasiLaper/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import main_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +23,5 @@ urlpatterns = [
     path('feedback/', include('feedback.urls')),
     path('order/', include('order.urls')),
     path('menuadmin/', include('menuadmin.urls')),
+    path('', main_page, name='main_page'),
 ]
